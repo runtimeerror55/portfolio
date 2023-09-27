7ac42a28271414b740290b74ac90c891f6cb382c
runtimeerror55 <runtimeerror55@gmail.com>
2023-09-28 01:18:24 +0530

    some changes

--- src/loaders/loaders.js
+++ src/loaders/loaders.js
@@ -2,7 +2,9 @@ import { defer } from "react-router";
 
 export const getProjects = async () => {
       try {
-            const response = await fetch("http://localhost:8080/projects");
+            const response = await fetch(
+                  "https://portfolio-backend-red.vercel.app/projects"
+            );
             console.log(response);
             const data = response.json();
             return data;
@@ -14,7 +16,7 @@ export const getProjects = async () => {
 export const getProject = async (request, params) => {
       try {
             const response = await fetch(
-                  `http://localhost:8080/projects/${params.id}`
+                  `https://portfolio-backend-red.vercel.app/projects/${params.id}`
             );
 
             const data = await response.json();
